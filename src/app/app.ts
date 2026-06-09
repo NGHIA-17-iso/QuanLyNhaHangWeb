1275f58079ba95e99fc6f2f2b66104040b87707b
NGHIA-17-iso <trongnghia314314@gmail.com>
2026-06-09 16:00:51 +0700

    push ban dau

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms'; // Thêm dòng này để chuẩn bị làm Form Đăng nhập/Đăng ký
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // Bản chất các bản mới đã tự bật chế độ này
+  imports: [RouterOutlet,
+    ReactiveFormsModule // Khai báo ReactiveFormsModule tại đây để kích hoạt [formGroup]
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
