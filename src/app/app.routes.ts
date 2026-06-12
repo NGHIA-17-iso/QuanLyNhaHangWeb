01fd77793fc52e3867d094b45733b4c25a28f6bc
NGHIA-17-iso <trongnghia314314@gmail.com>
2026-06-12 13:04:49 +0700

    xử lý các phần tính năng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
-import { RegisterComponent } from './features/auth/register/register'; // Nhớ check đúng tên file register.ts của bạn
-
+import { RegisterComponent } from './features/auth/register/register';
 import { AdminLayout } from './layouts/admin-layout/admin-layout';
 
+import { Revenue } from './features/auth/admin/user-management/revenue/revenue';
+import { Create } from './features/auth/admin/user-management/create/create';
+
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }, // THÊM DÒNG NÀY: Khai báo định tuyến trang đăng ký
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+
   {
     path: 'admin',
     component: AdminLayout,
@@ -16,14 +27,38 @@ export const routes: Routes = [
         path: 'user-management',
         loadComponent: () =>
           import('./features/auth/admin/user-management/user-management')
-          .then(m => m.UserManagement)
+            .then(m => m.UserManagement)
       },
 
       {
         path: 'create-employee',
+        component: Create
+      },
+
+      {
+        path: 'edit-employee/:id',
         loadComponent: () =>
-          import('./features/auth/admin/user-management/create/create')
-            .then(m => m.Create)
+          import('./features/auth/admin/user-management/edit-employee/edit-employee')
+            .then(m => m.EditEmployee)
+      },
+
+      {
+        path: 'table-management',
+        loadComponent: () =>
+          import('./features/auth/admin/user-management/table-management/table-management')
+            .then(m => m.TableManagement)
+      },
+
+      {
+        path: 'food-management',
+        loadComponent: () =>
+          import('./features/auth/admin/user-management/food-management/food-management')
+            .then(m => m.FoodManagement)
+      },
+
+      {
+        path: 'revenue',
+        component: Revenue
       },
 
       {
@@ -34,7 +69,16 @@ export const routes: Routes = [
 
     ]
   },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  
-  { path: '**', redirectTo: '/login' }
+
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
+
 ];
